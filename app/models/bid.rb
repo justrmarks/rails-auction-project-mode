178fe51214bb0ad8ed35722c0972d075218e09de
Ashwin Kannan <classifieddatabank@Ashwins-MacBook-Pro.local>
2019-10-03 18:20:49 -0500

    Commented the models

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -1,7 +1,9 @@
 class Bid < ApplicationRecord
+  # A Bid has a User that placed the bid and a Sale that it was placed on.
   belongs_to :user
   belongs_to :sale
 
+  # Delegate the username function to retrieve a Bid's placer's username.
   delegate :username, :to => :user
 
   # Return a formatted string representing the time that a Bid was placed.
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,45 +7,43 @@ class User < ApplicationRecord
   # A User that is a buyer has many bids.
   has_many :bids
 
-
-  # get_closed_sales_as_seller
+  # Get a list of the Sales a Buyer has Bid on.
   def get_sales_as_bidder
     self.bids.map {|bid| bid.sale}.select {|sale| sale.active}.uniq
   end
 
+  # Get a list of the Sales a Seller has posted.
   def open_sales
     self.sales.select {|sale| sale.active }
   end
 
+  # Get a list of a Seller's Sales that found a buyer.
   def get_successful_sales
     self.sales.select {|sale| !sale.active && sale.owner_id != self.id}
   end
 
+  # Get a list of the Sales whose auction a Buyer won.
   def won_sales
-    Sale.all.select do |sale|
-      !sale.active &&
-      sale.owner_id == self.id &&
-      sale.owner_id != sale.seller_id
-    end
+    Sale.all.select {|sale| !sale.active && sale.owner_id == self.id && self.id != sale.seller_id}
   end
 
-
+  # Gets the total revenue a Seller has made from successful Sales.
   def get_total_revenue
     self.get_successful_sales.sum {|sale| sale.get_last_bid.amount}
   end
 
+  # Gets the success rate of a Seller's Sales.
   def sale_success_rate
     self.get_successful_sales.count.to_f / self.sales.count
   end
 
+  # Get the Seller's Sale that received the most Bids.
   def most_popular_sale
     self.get_successful_sales.max_by {|sale| sale.bids.count}
   end
 
+  # Get a list of the Seller's Sales that were not Bid on.
   def get_unsuccessful_sales
     (self.sales - self.get_successful_sales).select {|sale| !sale.active}
   end
-
-
-
 end
